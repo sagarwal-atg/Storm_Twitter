4b7bd5a97881895b5d97bb24693451e12aefb3f6
shubhankar12 <shubhankar0109@gmail.com>
2016-07-16 23:39:15 -0700

    Storm basic topology

---
+++ lesson1/stage2/src/jvm/udacity/storm/spout/RandomSentenceSpout.java
@@ -0,0 +1,44 @@
+package udacity.storm.spout;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Map;
+import java.util.Random;
+
+public class RandomSentenceSpout extends BaseRichSpout {
+  SpoutOutputCollector _collector;
+  Random _rand;
+
+
+  @Override
+  public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+    _collector = collector;
+    _rand = new Random();
+  }
+
+  @Override
+  public void nextTuple() {
+    Utils.sleep(100);
+    String[] sentences = new String[]{
+      "  Pal bhar thahar jaao",
+      "Dil ye sambhal jaaye  Kaise tumhe roka karun",
+      "Meri taraf aata har gham phisal jaaye",
+      "Aankhon mein tum ko bharun   Bin bole baatein tumse karun",
+      "‘gar tum saath ho..   Agar tum saath ho "
+      };
+    String sentence = sentences[_rand.nextInt(sentences.length)];
+    _collector.emit(new Values(sentence));
+  }
+
+  @Override
+  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    declarer.declare(new Fields("sentence"));
+  }
+
+}
